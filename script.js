98cdf578f8eda2d323bb33e8880e12a5b1de1fdf
Cauã Paniagua <paniaguacaua@gmail.com>
2024-02-02 04:34:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,68 @@
+//VARIÁVEIS:
+const botaoPlayPause = document.getElementById("play-pause");
+const botaoAvancar = document.getElementById("proximo");
+const botaoVoltar = document.getElementById("anterior");
+const nomeCapitulo = document.getElementById("capitulo");
+const audioCapitulo = document.getElementById("audio-capitulo");
+
+const numeroCapitulos = 10;
+let taTocando = 0;
+let capituloAtual = 1;
+
+//FUNÇÕES
+function tocarFaixa() {
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove("bi-play-circle-fill");
+    botaoPlayPause.classList.add("bi-pause-circle-fill");
+}
+
+function pausarFaixa() {
+    audioCapitulo.pause();
+    botaoPlayPause.classList.remove("bi-pause-circle-fill");
+    botaoPlayPause.classList.add("bi-play-circle-fill");
+}
+
+function tocarOuPausar() {
+    if (taTocando === 0) {
+        tocarFaixa();
+        taTocando = 1;
+    } else {
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
+
+function trocarNomeFaixa() {
+    nomeCapitulo.innerText = "Capítulo " + capituloAtual;
+}
+
+function proximaFaixa() {
+    if (capituloAtual === numeroCapitulos) {
+        capituloAtual = 1;
+    } else {
+        capituloAtual = capituloAtual + 1
+    }
+
+    audioCapitulo.src = "books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa()
+}
+
+function voltarFaixa() {
+    if (capituloAtual === numeroCapitulos) {
+        capituloAtual = numeroCapitulos;
+    } else {
+        capituloAtual = capituloAtual - 1
+    }
+
+    audioCapitulo.src = "books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa()
+}
+
+//EVENTOS:
+botaoPlayPause.addEventListener("click", tocarOuPausar);
+botaoAvancar.addEventListener("click", proximaFaixa);
+botaoVoltar.addEventListener("click", voltarFaixa);
